19a012b913c8d9488b368056a5caf012fcf9f17b
Alexandre Gouy <contact@agouy.com>
2020-10-12 10:14:49 +0200

    removed "from" variable

--- main.R
+++ main.R
@@ -4,24 +4,22 @@ library(proxy)
 library(tidyr)
 
 do.rmsd <- function(df) {
-  df <- df[, !colnames(df) %in% ".ci"]
-  rmsd <- function(x, y) sqrt(mean((x + y)^2))
-  
+  df <- df[, !colnames(df) %in% ".ri"]
+  rmsd <- function(x, y) sqrt(mean((x + y) ^ 2))
   dist.mat <- proxy::dist(df, method = rmsd)
-  xy <- t(combn(1:ncol(dist.mat),2))
+  xy <- t(combn(1:ncol(dist.mat), 2))
   
-  out <- data.frame(
-    .ri = xy[, 1],
-    from = c(ctx$rselect()[[1]])[xy[, 1]],
-    to = c(ctx$rselect()[[1]])[xy[, 2]],
+  df_out <- data.frame(
+    .ri = xy[, 1] - 1,
+    dist_to = c(ctx$rselect()[[1]])[xy[, 2]],
     dist = c(dist.mat)
   )
-  return(out)
+  return(df_out)
 }
 
 (ctx = tercenCtx())  %>% 
   select(.y, .ri, .ci) %>% 
-  spread(.ri, .y) %>%
+  spread(.ci, .y) %>%
   do(do.rmsd(.)) %>%
   ctx$addNamespace() %>%
   ctx$save()
